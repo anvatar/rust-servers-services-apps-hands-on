6f815a677858edb53853b77048e22319b0f8b847
HongJoon Ahn <anvatar@gmail.com>
2025-10-25 18:39:54 +0900

    Implement tcpclient (iteration 2)

--- scenario1/tcpclient/src/main.rs
+++ scenario1/tcpclient/src/main.rs
@@ -1,5 +1,10 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() {
-    let _stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+    let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+    println!("Got response from server:{:?}", str::from_utf8(&buffer).unwrap());
 }
