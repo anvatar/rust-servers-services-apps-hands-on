617569e4f5e9b7c02ff21bffbfd0d2ecb84fa7ab
HongJoon Ahn <anvatar@gmail.com>
2025-10-30 23:01:27 +0900

    Declare struct: `httpresponse.HttpResponse`

---
+++ scenario1/http/src/httpresponse.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct HttpResponse<'a> {
+    version: &'a str,
+    status_code: &'a str,
+    status_text: &'a str,
+    headers: Option<HashMap<&'a str, &'a str>>,
+    body: Option<String>,
+}
