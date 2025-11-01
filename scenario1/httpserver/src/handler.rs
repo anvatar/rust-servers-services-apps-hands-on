df3ad2dd52580e418a2f4c9aac2c3659590baa47
HongJoon Ahn <anvatar@gmail.com>
2025-11-01 20:37:53 +0900

    Define struct `httpserver::WebServiceHandler`

--- scenario1/httpserver/src/handler.rs
+++ scenario1/httpserver/src/handler.rs
@@ -1,4 +1,5 @@
 use http::{httprequest::HttpRequest, httpresponse::HttpResponse};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::{env, fs};
 
@@ -45,4 +46,38 @@ impl Handler for StaticPageHandler {
             },
         }
     }
+}
+
+pub struct WebServiceHandler;
+impl Handler for WebServiceHandler {
+    fn handle(req: &HttpRequest) -> HttpResponse {
+        let http::httprequest::Resource::Path(s) = &req.resource;
+        let route: Vec<&str> = s.split("/").collect();
+        match route[2] {
+            "shipping" if route.len() > 2 && route[3] == "orders" => {
+                let body = Some(serde_json::to_string(&Self::load_json()).unwrap());
+                let mut headers: HashMap<&str, &str> = HashMap::new();
+                headers.insert("Content-Type", "application/json");
+                HttpResponse::new("200", Some(headers), body)
+            }
+            _ => HttpResponse::new("404", None, Self::load_file("404.html")),
+        }
+    }
+}
+impl WebServiceHandler {
+    fn load_json() -> Vec<OrderStatus> {
+        let default_path = format!("{}/data", env!("CARGO_MANIFEST_DIR"));
+        let data_path = env::var("DATA_PATH").unwrap_or(default_path);
+        let full_path = format!("{}/{}", data_path, "orders.json");
+        let json_contents = fs::read_to_string(full_path);
+        let orders: Vec<OrderStatus> = serde_json::from_str(json_contents.unwrap().as_str()).unwrap();
+        orders
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct OrderStatus {
+    order_id: i32,
+    order_date: String,
+    order_status: String,
 }
