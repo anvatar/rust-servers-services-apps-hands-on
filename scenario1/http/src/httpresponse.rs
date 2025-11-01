b7c5a4d0fdfc4350ae4560bbcff8f9d215e231df
HongJoon Ahn <holden.ahn@linecorp.com>
2025-11-01 14:37:48 +0900

    Add tests for  `httpresponse::HttpResponse`

--- scenario1/http/src/httpresponse.rs
+++ scenario1/http/src/httpresponse.rs
@@ -94,3 +94,65 @@ impl<'a> From<HttpResponse<'a>> for String {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_response_struct_creation_200() {
+        let response_actual = HttpResponse::new(
+            "200",
+            None,
+            Some("Item was shipped on 21st Dec 2020".into()),
+        );
+        let response_expected = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "200",
+            status_text: "OK",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("Item was shipped on 21st Dec 2020".into()),
+        };
+        assert_eq!(response_expected, response_actual);
+    }
+    #[test]
+    fn test_response_struct_creation_404() {
+        let response_actual = HttpResponse::new(
+            "404",
+            None,
+            Some("Item was shipped on 21st Dec 2020".into()),
+        );
+        let response_expected = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "404",
+            status_text: "Not Found",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("Item was shipped on 21st Dec 2020".into()),
+        };
+        assert_eq!(response_expected, response_actual);
+    }
+    #[test]
+    fn test_http_response_creation() {
+        let response = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "404",
+            status_text: "Not Found",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("Item was shipped on 21st Dec 2020".into()),
+        };
+        let http_string_actual = String::from(response);
+        let http_string_expected = "HTTP/1.1 404 Not Found\r\nContent-Type:text/html\r\nContent-Length: 33\r\n\r\nItem was shipped on 21st Dec 2020";
+        assert_eq!(http_string_expected, http_string_actual);
+    }
+}
