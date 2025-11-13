7a27071fc324f600752f176c3a4960f30742bc61
HongJoon Ahn <anvatar@gmail.com>
2025-11-13 23:21:47 +0900

    Handle GET /health requests

--- ezytutors/tutor-nodb/src/bin/basic-server.rs
+++ ezytutors/tutor-nodb/src/bin/basic-server.rs
@@ -1,5 +1,5 @@
 use std::io;
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -9,5 +9,10 @@ async fn main() -> io::Result<()> {
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
 
-pub fn general_routes(_cfg: &mut web::ServiceConfig) { // "pub" can be omitted
+pub fn general_routes(cfg: &mut web::ServiceConfig) { // "pub" can be omitted
+    cfg.route("/health", web::get().to(health_check_handler));
+}
+
+pub async fn health_check_handler() -> impl Responder { // "pub" can be omitted
+    HttpResponse::Ok().json("Hello. EzyTutors is alive and kicking")
 }
