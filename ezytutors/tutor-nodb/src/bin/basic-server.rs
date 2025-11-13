a132e02657a7f5cd73d8b4b83c969d4ae15382dd
HongJoon Ahn <anvatar@gmail.com>
2025-11-13 23:16:02 +0900

    Define empty routes

--- ezytutors/tutor-nodb/src/bin/basic-server.rs
+++ ezytutors/tutor-nodb/src/bin/basic-server.rs
@@ -1,10 +1,13 @@
 use std::io;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     // "move" can be omitted here because there's nothing to capture
-    let app = move || App::new();
+    let app = move || App::new().configure(general_routes);
 
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
+
+pub fn general_routes(_cfg: &mut web::ServiceConfig) { // "pub" can be omitted
 }
