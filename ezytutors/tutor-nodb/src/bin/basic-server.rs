46ad122e7365b4d3f5ce0df385ade3cd25bb23bf
HongJoon Ahn <anvatar@gmail.com>
2025-11-13 23:01:44 +0900

    Start trivial Actix HTTP server

--- ezytutors/tutor-nodb/src/bin/basic-server.rs
+++ ezytutors/tutor-nodb/src/bin/basic-server.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use actix_web::{App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    // "move" can be omitted here because there's nothing to capture
+    let app = move || App::new();
+
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
