252ad4118ba7625040afc5c8534ee2bba3083394
HongJoon Ahn <anvatar@gmail.com>
2025-11-21 21:07:21 +0900

    Add test for new_course

--- ezytutors/tutor-nodb/src/handlers.rs
+++ ezytutors/tutor-nodb/src/handlers.rs
@@ -34,3 +34,27 @@ pub async fn new_course(
     app_state.courses.lock().unwrap().push(new_course);
     HttpResponse::Ok().json("Added course")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use std::sync::Mutex;
+
+    #[actix_rt::test]
+    async fn post_course_test() {
+        let course = web::Json(Course {
+            tutor_id: 1,
+            course_name: "Hello, this is test course".into(),
+            course_id: None,
+            posted_time: None,
+        });
+        let app_state = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+        let resp = new_course(course, app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
