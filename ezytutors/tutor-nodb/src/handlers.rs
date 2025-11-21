68059c93b095dbf2d23852153706d10d472ae3fd
HongJoon Ahn <anvatar@gmail.com>
2025-11-21 21:14:57 +0900

    Add handler: new_course()

--- ezytutors/tutor-nodb/src/handlers.rs
+++ ezytutors/tutor-nodb/src/handlers.rs
@@ -1,5 +1,7 @@
 use super::state::AppState;
+use crate::models::Course;
 use actix_web::{HttpResponse, web};
+use chrono::Utc;
 
 pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
     let health_check_response = &app_state.health_check_response;
@@ -8,4 +10,27 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
     let response = format!("{} {} times", health_check_response, visit_count);
     *visit_count += 1; // DerefMut is overridden for MutexGuard
     HttpResponse::Ok().json(&response) // "&" can be omitted because response is no more used here
+}
+
+pub async fn new_course(
+    new_course: web::Json<Course>,
+    app_state: web::Data<AppState>,
+) -> HttpResponse {
+    println!("Received new course");
+    let course_count_for_user = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.tutor_id == new_course.tutor_id)
+        .count();
+    let new_course = Course {
+        tutor_id: new_course.tutor_id,
+        course_id: Some((course_count_for_user + 1) as i32),
+        course_name: new_course.course_name.clone(),
+        posted_time: Some(Utc::now().naive_utc()),
+    };
+    app_state.courses.lock().unwrap().push(new_course);
+    HttpResponse::Ok().json("Added course")
 }
